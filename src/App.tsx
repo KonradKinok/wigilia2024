b9bfffcb7dafce80c59121e54683722ea6a729af
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-11-13 19:16:46 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>React + Vit + TS</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/globalFunctions/functions.ts
@@ -0,0 +1,134 @@
+"use strict";
+
+/**
+ *
+ * @param {string} tagDivAndIdExampleTheory
+ * @param {string} tagDivAndIdExamplePractice
+ */
+export function htmlTagViewExample(
+  tagDivAndIdExampleTheory: string,
+  tagDivAndIdExamplePractice: string,
+  className: string = "example-theory-pre"
+): void {
+  const example_theory = document.querySelector(tagDivAndIdExampleTheory);
+  const example_practice = document.querySelector(tagDivAndIdExamplePractice);
+
+  if (example_theory && example_practice) {
+    const phrase = document.createElement("pre");
+    phrase.classList.add(className);
+    const examplePracticeInnerHtml = example_practice.innerHTML.split("  ").join("");
+
+    phrase.textContent = `HTML: ${examplePracticeInnerHtml}`;
+    example_theory.prepend(phrase);
+  }
+}
+
+/**
+ * LocalStorage saveLocalStorage
+ * @param {string} key
+ * @param {object} value
+ */
+export const saveLocalStorage = (key: string, value: object): void => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Set state error: ", error.message);
+    } else {
+      console.error("An unexpected error occurred");
+    }
+  }
+};
+
+/**
+ * localStorage load
+ * @param {string} key
+ * @returns {object | undefined}
+ */
+export const loadLocalStorage = (key: string): object | undefined => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Set state error: ", error.message);
+    } else {
+      console.error("An unexpected error occurred");
+    }
+  }
+};
+
+/**
+ * getRandomElement
+ * @param {Array<T>} array
+ * @returns {T}
+ */
+export function getRandomElement<T>(array: Array<T>): T {
+  // Generuj losowy indeks od 0 do array.length - 1
+  const randomIndex = Math.floor(Math.random() * array.length);
+  // Zwróć wybrany element
+  return array[randomIndex];
+}
+
+/**
+ * getRandomHexColor
+ * @returns {string}
+ */
+export function getRandomHexColor(): string {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
+
+/**
+ * getRandomColorRgba
+ * @returns {string}
+ */
+export function getRandomColorRgba(): string {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const a = 0.2 + Math.random() * 0.3;
+  return `rgba(${r}, ${g}, ${b}, ${a.toFixed(2)})`;
+}
+
+/**
+ * capitalizeFirstLetter
+ * @param {string} str
+ * @returns {string}
+ */
+export const capitalizeFirstLetter = (str: string): string => {
+  return str ? str.charAt(0).toUpperCase() + str.slice(1) : str;
+};
+
+export const toLowerCaseFirstLetter = (str: string): string => {
+  return str ? str.charAt(0).toLowerCase() + str.slice(1) : str;
+};
+
+//formatDate
+export function formatDate(dateStr: string):string {
+  const date = new Date(dateStr);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
+export function formatDateAndHour(dateStr: string):string {
+  const date = new Date(dateStr);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
+  const year = date.getFullYear();
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${day}.${month}.${year}y. ${hour}:${minutes}:${seconds}`;
+};
+
+
+export function addDays(date: Date, days: number): Date {
+  const result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
